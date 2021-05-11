d33d75f2d384ae04fcb3e2e6d5f583411278a5d9
protosse <liuliu1994@gmail.com>
2021-05-11 15:58:53 +0800

    :hammer: 561 for loop

--- 561.数组拆分-i/561.数组拆分-i.go
+++ 561.数组拆分-i/561.数组拆分-i.go
@@ -62,10 +62,8 @@ import (
 func arrayPairSum(nums []int) int {
 	sort.Ints(nums)
 	result := 0
-	for i := 0; i < len(nums); i++ {
-		if i%2 == 0 {
-			result += nums[i]
-		}
+	for i := 0; i < len(nums); i += 2 {
+		result += nums[i]
 	}
 	return result
 }
