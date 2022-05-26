a86d55216d1d866772dc45e20e4135a0d51ad56f
protosse <liuliu1994@gmail.com>
2022-05-26 18:12:50 +0800

    merge-two-sorted-lists

---
+++ 21.merge-two-sorted-lists/21.合并两个有序链表.py
@@ -0,0 +1,89 @@
+#
+# @lc app=leetcode.cn id=21 lang=python3
+#
+# [21] 合并两个有序链表
+#
+# https://leetcode.cn/problems/merge-two-sorted-lists/description/
+#
+# algorithms
+# Easy (66.66%)
+# Likes:    2450
+# Dislikes: 0
+# Total Accepted:    1M
+# Total Submissions: 1.5M
+# Testcase Example:  '[1,2,4]\n[1,3,4]'
+#
+# 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：l1 = [1,2,4], l2 = [1,3,4]
+# 输出：[1,1,2,3,4,4]
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：l1 = [], l2 = []
+# 输出：[]
+# 
+# 
+# 示例 3：
+# 
+# 
+# 输入：l1 = [], l2 = [0]
+# 输出：[0]
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 两个链表的节点数目范围是 [0, 50]
+# -100 
+# l1 和 l2 均按 非递减顺序 排列
+# 
+# 
+#
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # 虚拟头节点
+        head = ListNode()
+        cur = head
+        i = list1
+        j = list2
+
+        while i != None and j != None:
+            # 比较两个节点的值，将较小的节点接到cur的后面
+            if i.val <= j.val:
+                cur.next = i
+                i = i.next
+            else:
+                cur.next = j
+                j = j.next
+                # 不断向前
+            cur = cur.next
+        # 如果有一个为空了，那么直接将另一个链表接到cur的后面
+        cur.next = i if i != None else j
+        return head.next
+        
+# @lc code=end
