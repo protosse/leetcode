4f9df54cd6cd37090228679835121b1c58af4cd6
protosse <liuliu1994@gmail.com>
2022-05-30 23:26:01 +0800

    double pointer and slow and fast pointer

---
+++ 19.remove-nth-node-from-end-of-list/19.删除链表的倒数第-n-个结点.py
@@ -0,0 +1,87 @@
+#
+# @lc app=leetcode.cn id=19 lang=python3
+#
+# [19] 删除链表的倒数第 N 个结点
+#
+# https://leetcode.cn/problems/remove-nth-node-from-end-of-list/description/
+#
+# algorithms
+# Medium (44.10%)
+# Likes:    2061
+# Dislikes: 0
+# Total Accepted:    800.7K
+# Total Submissions: 1.8M
+# Testcase Example:  '[1,2,3,4,5]\n2'
+#
+# 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：head = [1,2,3,4,5], n = 2
+# 输出：[1,2,3,5]
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：head = [1], n = 1
+# 输出：[]
+# 
+# 
+# 示例 3：
+# 
+# 
+# 输入：head = [1,2], n = 1
+# 输出：[1]
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 链表中结点的数目为 sz
+# 1 <= sz <= 30
+# 0 <= Node.val <= 100
+# 1 <= n <= sz
+# 
+# 
+# 
+# 
+# 进阶：你能尝试使用一趟扫描实现吗？
+# 
+#
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        p1 = head
+        p2 = head
+        # 先找到倒数第n+1个结点
+        for _ in range(n + 1):
+            if p1:
+                p1 = p1.next
+            else:
+                # 如果为空说明链表小于等于n个结点
+                # 直接删除头节点
+                return head.next
+        while p1:
+            p1 = p1.next
+            p2 = p2.next
+        p2.next = p2.next.next
+        return head
+# @lc code=end
+
