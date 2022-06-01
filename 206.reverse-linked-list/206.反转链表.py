fae4ef370000768370a1bef3375aab9e5cd0337a
protosse <liuliu1994@gmail.com>
2022-06-01 17:33:34 +0800

    reverse-linked-list

---
+++ 206.reverse-linked-list/206.反转链表.py
@@ -0,0 +1,93 @@
+#
+# @lc app=leetcode.cn id=206 lang=python3
+#
+# [206] 反转链表
+#
+# https://leetcode.cn/problems/reverse-linked-list/description/
+#
+# algorithms
+# Easy (72.95%)
+# Likes:    2550
+# Dislikes: 0
+# Total Accepted:    1.1M
+# Total Submissions: 1.5M
+# Testcase Example:  '[1,2,3,4,5]'
+#
+# 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+# 
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：head = [1,2,3,4,5]
+# 输出：[5,4,3,2,1]
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：head = [1,2]
+# 输出：[2,1]
+# 
+# 
+# 示例 3：
+# 
+# 
+# 输入：head = []
+# 输出：[]
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 链表中节点的数目范围是 [0, 5000]
+# -5000 
+# 
+# 
+# 
+# 
+# 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+# 
+# 
+# 
+#
+
+from operator import ne
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList_iter(self, head: ListNode) -> ListNode:
+        pre = None
+        cur = head
+        while cur:
+            next = cur.next
+            cur.next = pre 
+            pre = cur
+            cur = next
+        return pre
+
+    def reverseList(self, head: ListNode) -> ListNode:
+        if head == None or head.next == None:
+            return head
+        last = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return last
+            
+# @lc code=end
+
