52d3e85fab17150e3c35fc09f49f6696b9bca35e
protosse <liuliu1994@gmail.com>
2022-06-02 16:09:21 +0800

    valid-palindrome

---
+++ 125.valid-palindrome/125.验证回文串.py
@@ -0,0 +1,72 @@
+#
+# @lc app=leetcode.cn id=125 lang=python3
+#
+# [125] 验证回文串
+#
+# https://leetcode.cn/problems/valid-palindrome/description/
+#
+# algorithms
+# Easy (46.92%)
+# Likes:    530
+# Dislikes: 0
+# Total Accepted:    365.9K
+# Total Submissions: 779.9K
+# Testcase Example:  '"A man, a plan, a canal: Panama"'
+#
+# 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+# 
+# 说明：本题中，我们将空字符串定义为有效的回文串。
+# 
+# 
+# 
+# 示例 1:
+# 
+# 
+# 输入: "A man, a plan, a canal: Panama"
+# 输出: true
+# 解释："amanaplanacanalpanama" 是回文串
+# 
+# 
+# 示例 2:
+# 
+# 
+# 输入: "race a car"
+# 输出: false
+# 解释："raceacar" 不是回文串
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 1 
+# 字符串 s 由 ASCII 字符组成
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            l = s[left].lower()
+            r = s[right].lower()
+
+            if not l.isalnum():
+                left += 1
+                continue
+
+            if not r.isalnum():
+                right -= 1
+                continue
+
+            if l != r:
+                return False
+
+            left += 1
+            right -= 1
+        return True
+# @lc code=end
