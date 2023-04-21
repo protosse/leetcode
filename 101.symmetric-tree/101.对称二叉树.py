20ed0a87497958eb780ef48818829219b7f6d4db
protosse <liuliu1994@gmail.com>
2023-04-21 17:06:31 +0800

    symmetric-tree

---
+++ 101.symmetric-tree/101.对称二叉树.py
@@ -0,0 +1,76 @@
+#
+# @lc app=leetcode.cn id=101 lang=python3
+#
+# [101] 对称二叉树
+#
+# https://leetcode.cn/problems/symmetric-tree/description/
+#
+# algorithms
+# Easy (58.71%)
+# Likes:    2390
+# Dislikes: 0
+# Total Accepted:    815K
+# Total Submissions: 1.4M
+# Testcase Example:  '[1,2,2,3,4,4,3]'
+#
+# 给你一个二叉树的根节点 root ， 检查它是否轴对称。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：root = [1,2,2,3,4,4,3]
+# 输出：true
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：root = [1,2,2,null,3,null,3]
+# 输出：false
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 树中节点数目在范围 [1, 1000] 内
+# -100 <= Node.val <= 100
+# 
+# 
+# 
+# 
+# 进阶：你可以运用递归和迭代两种方法解决这个问题吗？
+# 
+#
+
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.isMirror(root, root)
+
+    def isMirror(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p == None and q == None:
+            return True
+        if p == None or q == None:
+            return False
+        return p.val == q.val and self.isMirror(p.left, q.right) and self.isMirror(p.right, q.left)
+# @lc code=end
+
