8147f78f1b1b991036d225a9b8021ca1ccb13c70
protosse <liuliu1994@gmail.com>
2025-03-12 11:09:44 +0800

    202 Supplement

--- 19.删除链表的倒数第 N 个结点/19.py
+++ 19.删除链表的倒数第 N 个结点/19.py
@@ -70,7 +70,8 @@ class ListNode:
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # 虚拟头结点
+        # 虚拟头结点，防止出现空指针的情况
+        # 比如说链表总共有 5 个节点，删除倒数第 5 个节点，也就是第一个节点，那按照算法逻辑，应该首先找到倒数第 6 个节点。但第一个节点前面已经没有节点了，这就会出错
         dummy = ListNode(-1)
         dummy.next = head
         # 删除倒数第 n 个，要先找倒数第 n + 1 个节点
